09e715080361527eb615d3cc734ebac6a3cfaf34
Rafael <arvenegasd@gmail.com>
2020-05-28 15:20:01 +0200

    first deploy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 const routes = require('./routes/index.js');
 const app = express();
 
@@ -9,6 +10,7 @@ app.set('port', process.env.PORT || 3000);
 
 //middlewares
 app.use (morgan('dev'));
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 
 //routes
