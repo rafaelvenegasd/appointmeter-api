f3850366ef67ec84efe6301c950cb5e90af80915
Rafael <arvenegasd@gmail.com>
2020-05-30 08:42:34 +0200

    testing login

--- routes/index.js
+++ routes/index.js
@@ -10,18 +10,27 @@ firebase.initializeApp({
 
 const db = firebase.database();
 
-router.get('/', (req, res) => {
-    console.log('index working');
-    db.ref('users').once('value', (snapshot) => {
-        return data = snapshot.val();
-    })
-    res.send({users: data});
-})
+// router.get('/', (req, res) => {
+//     console.log('index working');
+//     db.ref('users').once('value', (snapshot) => {
+//         return data = snapshot.val();
+//     })
+//     res.send({users: data});
+// })
 
 router.post('/signup/users', (req, res) => {
     console.log(req.body);
     res.send(req.body);
     db.ref('users').push(req.body);
+})
+
+router.post('/login/users', (req, res) => {
+    // const snapshot = req.body;
+    db.ref('users').once('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+    })
+    // res.send({users: data});
 })
 
 module.exports = router;
